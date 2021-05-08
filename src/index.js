0cd58c40fff327ef8c9b576a03e96a068e93010d
Tulasisree <sree.mulagapaka@gmail.com>
2021-05-09 00:45:52 +0530

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import Countdown from 'react-countdown';
+import  Timer from 'react-compound-timer';
+import './styles.css';
+ 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  /*<div>
+     <Countdown date={Date.now() + 10000} />
+     <button>START</button>
+  </div>*/
+  <Timer
+    initialTime={prompt("Enter the value for timer in seconds")*1000}
+    direction="backward"
+    startImmediately={false}
+>
+    {({ start, resume, pause, stop, reset, timerState,initialTime}) => (
+        <React.Fragment>
+        <div className="buttons top">
+        <button onClick={start}>Start</button>
+        <button onClick={stop}>Stop</button>
+        </div>
+            <div className="timer">
+              <div className="t"><Timer.Hours /></div>  :
+               <div className="t"><Timer.Minutes /></div> :
+               <div className="t"><Timer.Seconds /></div> 
+            </div>
+           
+            <br />
+            <div className="buttons bottom">
+                <button onClick={pause}>Pause</button>
+                <button onClick={resume}>Resume</button>  
+                <button onClick={reset}>Reset</button>
+            </div>
+        </React.Fragment>
+    )}
+</Timer>
+  ,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
